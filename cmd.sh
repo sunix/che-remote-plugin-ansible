fe143ecd19e3be68e6400a246a7259155c9cc628
Sun Seng David TAN <sutan@redhat.com>
2019-09-24 16:02:42 -0400

    split entrypoint to separate passwd changes and theia runtime start
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

---
+++ cmd.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Copyright (c) 2018-2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+
+cd ${HOME}
+
+[ -f "/before-start.sh" ] && . "/before-start.sh"
+
+# run theia endpoint
+node /home/theia/lib/node/plugin-remote.js &
+
+PID=$!
+
+# See: http://veithen.github.io/2014/11/16/sigterm-propagation.html
+wait ${PID}
+wait ${PID}
+EXIT_STATUS=$?
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
