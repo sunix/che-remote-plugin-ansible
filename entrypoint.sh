9bc179c1c879f9b8d6fa269cf969c89e1530f5cc
Sun Seng David TAN <sutan@redhat.com>
2019-09-24 17:33:34 -0400

    Fixing entrypoint with the right home folder
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,15 +18,15 @@ if ! grep -Fq "${USER_ID}" /etc/passwd; then
     # current user is an arbitrary
     # user (its uid is not in the
     # container /etc/passwd). Let's fix that
-    cat ${HOME}/passwd.template | \
+    cat /home/theia/passwd.template | \
     sed "s/\${USER_ID}/${USER_ID}/g" | \
     sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
-    sed "s/\${HOME}/\/home\/theia/g" > /etc/passwd
+    sed "s/\${HOME}/\/projects/g" > /etc/passwd
 
-    cat ${HOME}/group.template | \
+    cat /home/theia/group.template | \
     sed "s/\${USER_ID}/${USER_ID}/g" | \
     sed "s/\${GROUP_ID}/${GROUP_ID}/g" | \
-    sed "s/\${HOME}/\/home\/theia/g" > /etc/group
+    sed "s/\${HOME}/\/projects/g" > /etc/group
 fi
 
 # Grant access to projects volume in case of non root user with sudo rights
